92f44b89c6eeaf3a6487140fd05a91c02fe0b099
chikamasuda <66733811+chikamasuda@users.noreply.github.com>
2021-09-05 21:10:05 +0900

    19.テンプレート文字列まで終了

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /**
- * const,letなどの変数宣言
+ * const,let等の変数宣言
  */
 // var val1 = "var変数";
 // console.log(val1);
@@ -16,20 +16,20 @@
 // console.log(val3);
 
 // //const変数は上書き不可
-// //
+// //val3 = "const変数を上書き";
 
 // const val3 = "const変数を再宣言";
 
+//constで定義したオブジェクトはプロパティの変更が可能
 // const val4 = {
 //   name: "じゃけえ",
-//   age: 28,
+//   age: 28
 // };
-// //constで定義したオブジェクトは、プロパティの変更が可能。
 // val4.name = "jak";
 // val4.address = "Hiroshima";
 // console.log(val4);
 
-//constで定義した配列は、プロパティの変更が可能。
+//constで定義した配列はプロパティの変更が可能
 // const val5 = ['dog', 'cat'];
 // val5[0] = "bird";
 // val5.push("monkey");
@@ -38,67 +38,14 @@
 /**
  * テンプレート文字列
  */
-// const name = "じゃけえ";
-// const age = 28;
+const name = "じゃけぇ";
+const age = 28;
+//「私の名前はじゃけぇです。年齢は28歳です。」
 
-// //私の名前はじゃけえです。年齢は28歳です。
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
 
-// //従来の方法
-// const message1 = "私の名前は" + name + "です。年齢は" + age +  "です";
-// console.log(message1);
-
-// //テンプレート文字列を用いた方法
-// const message2 = `私の名前は${name}です。年齢は${age}です。`;
-// console.log(message2);
-
-/**
- * アロー関数
- */
-//従来の関数
-// function func1(str) {
-//   return str;
-// }
-// const func1 = function (str) {
-//   return str;
-// };
-
-// console.log(func1("func1です。"));
-
-// //アロー関数
-// const func2 = (str) => str;
-
-// console.log(func2("func2です。"));
-
-// const func3 = (num1, num2) => num1 + num2;
-
-// console.log(func3(10, 20));
-
-// /**
-//  * 分割代入
-//  */
-// const myProfile = {
-//   name: "じゃけえ",
-//   age: 28
-// };
-
-// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
-// console.log(message1);
-
-// const {name, age} = myProfile;
-// const message2 = `名前は${name}です。年齢は${age}です。`;
-// console.log(message2);
-
-// const myProfile = ["じゃけえ", 28];
-
-// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-// console.log(message3);
-
-// const [name, age] = myProfile;
-// const message4 = `名前は${name}です。年齢は${age}です。`;
-// console.log(message4);
-
-/**
- * デフォルト値
- */
-const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
-sayHello("じゃけえ");
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
